ca3345816e059e781618089f93c2c3e42cbe2209
Minh Ngọc <ngnm1009@gmail.com>
2025-06-24 16:38:40 +0700

    chore(): fix type

--- src/Utils/margin.ts
+++ src/Utils/margin.ts
@@ -1,8 +1,8 @@
-import type { ViewStyle } from 'react-native';
+// import type { ViewStyle } from 'react-native';
 import type { MarginProps } from '../types';
 import { calculateValue, removeUndefined } from './other';
 
-export const calculateMargin: (p: MarginProps) => ViewStyle = (props) => {
+export const calculateMargin: (p: MarginProps) => any = (props) => {
   const { margin, ...rest } = props;
   return {
     ...calculateValue(margin, 'margin'),
